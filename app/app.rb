ce07c1ce02777ecbb846626d9c2786b7926f6b21
davidbebb <davidbebb@gmail.com>
2015-09-22 15:50:13 +0100

    updated some requires

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
-require './app/models/link.rb'
+require './lib/data_mapper_setup'
+
 
 class Bookmark_manager < Sinatra::Base
   get '/' do
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,3 @@
-require './lib/data_mapper_setup'
 
 class Link
   include DataMapper::Resource
