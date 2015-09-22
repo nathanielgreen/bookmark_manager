8eaed6f44db01a6bb7cc0dbb4487c6766d138ae1
Nathaniel Green <nathanielgreen@Nathaniels-MacBook-Pro.local>
2015-09-22 14:04:14 +0100

    New test added creating_links_spec

--- app/app.rb
+++ app/app.rb
@@ -10,6 +10,10 @@ class Bookmark_manager < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/links/new' do
+    
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
